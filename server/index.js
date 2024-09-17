0966e87112e8985ca331b324cacc24596cbafc5a
MuhammadSarimWaseem <sarimwaseem84@gmail.com>
2024-09-18 01:20:16 +0500

    updated

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 const stripe = require('stripe')(process.env.STRIPE);
 const app = express();
 
-app.use(cors());
+app.use(cors({ origin: "https://stripe-integration-zeta.vercel.app/" }));
 app.use(express.json());
 
 app.get('/', (req, res) => {
