a60422c6afeaa3c94c27b9548fedc44c72b2296d
MuhammadSarimWaseem <sarimwaseem84@gmail.com>
2024-09-18 01:34:30 +0500

    updated

--- server/index.js
+++ server/index.js
@@ -8,10 +8,6 @@ const app = express();
 app.use(cors({ origin: "https://stripe-integration-zeta.vercel.app/" }));
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello World');
-});
-
 app.post('/payment', async (req, res) => {
   const { cartItem } = req.body;
 
